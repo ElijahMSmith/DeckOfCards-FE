05e70eb3b532166e165463376843aa3106c507d2
root <root@Austin-PC.localdomain>
2022-03-31 17:55:27 -0400

    tied to BE

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+import Navigation from './src/components/Navigation';
+import {AuthProvider} from './src/context/AuthContext';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <StatusBar backgroundColor="#35654d" />
+      <Navigation />
+    </AuthProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
