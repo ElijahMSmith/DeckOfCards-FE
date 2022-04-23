20433834018ca61fcfec8d979712d9f12913a5c2
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2022-04-22 21:09:43 -0400

    Begin to add layout for adding presets

--- src/screens/LobbyScreen.js
+++ src/screens/LobbyScreen.js
@@ -3,13 +3,14 @@ import { Text, TextInput, TouchableOpacity, View, StyleSheet, Switch, Pressable,
 import Spinner from 'react-native-loading-spinner-overlay';
 import { AuthContext } from '../context/AuthContext';
 import io from "socket.io-client";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const LobbyScreen = ({ navigation }) => {
     const {userInfo, isLoading, logout, gameStateC, setGameStateC, socketC, setSocketC} = useContext(AuthContext);
     const [state, setState] = useState({});
     const [joinCode, setJoinCode] = useState("");
     const [socket, setSocket] = useState(socketC);
-    // connect to server
+    const [presetName, setName] = useState('');
 
     const startGame = () => {
         socket.emit(
@@ -31,6 +32,12 @@ const LobbyScreen = ({ navigation }) => {
                 navigation.navigate('Game');
         });
     }
+
+    const savePreset = () => {
+        
+    }
+
+    
 
     const [isDealer, setDealer] = useState(false);
     const toggleDealer = () => setDealer(previousState => !previousState);
@@ -124,6 +131,30 @@ const LobbyScreen = ({ navigation }) => {
                 value={isClubs}
             />
 
+            <TextInput
+                style={styles.input}
+                value={presetName}
+                placeholder="Rule Preset Name"
+                onChangeText={text => setName(text)}
+                secureTextEntry
+            />
+
+            <Pressable style={styles.smallButton} onPress={savePreset}>
+               <Text style= {styles.smallButtonText}>Save Preset</Text>
+            </Pressable>
+
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Saved Presets
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
             <Pressable style={styles.button} onPress={startGame}>
                <Text style= {styles.buttonText}>Start the Game!</Text>
             </Pressable>
@@ -171,8 +202,31 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         fontSize: 16
     },
+    smallButton: {
+        backgroundColor: 'red',
+        width: '25%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginBottom: 20,
+        borderRadius: 10,
+        padding: 15
+    },
+    smallButtonText: {
+        color: 'black',
+        fontWeight: '700',
+        fontSize: 14
+    },
     link: {
         color: 'red',
+    },
+    input: {
+        marginBottom: 12,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        paddingHorizontal: 14,
+        backgroundColor: 'white',
     },
 });
 
